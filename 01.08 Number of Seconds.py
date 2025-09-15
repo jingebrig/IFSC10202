d2feb89c70ca0ae2f4540e56cea0b7729c8333ee
jingebrig <jingebrig@ualr.edu>
2025-09-15 15:08:38 +0000

    update

--- 01.08 Number of Seconds.py
+++ 01.08 Number of Seconds.py
@@ -3,3 +3,12 @@
 #Enter Minutes: 38
 #Enter Seconds: 11
 #2291
+
+num_min = int(input("Enter Minutes: "))
+num_sec = int(input("Enter Seconds: "))
+print(num_min*60+num_sec)
+
+num_min = int(input("Enter Minutes: "))
+min = num_min*60
+sec = int(input("Enter Seconds: "))
+print(min+sec)
--- 01.09 Miles In a Kilometer.py
+++ 01.09 Miles In a Kilometer.py
@@ -2,5 +2,12 @@
 #Calculate and display the number of miles in the kilometers.
 #Hint: Use the float() function to convert your input to an floating number.
 #Hint: There are 1.61 kilometers in a mile.
+
 #Enter Kilometers: 5
 #3.105590062111801
+
+km = float(input("Enter kilometers: "))
+m = km/1.61
+
+print(m)
+
