4b8da6c08322b5cb608492f23784ec000b02b9b5
jingebrig <jingebrig@ualr.edu>
2025-10-01 14:54:56 +0000

    update

--- 07.03 Two Halves.py
+++ 07.03 Two Halves.py
@@ -1,4 +1,6 @@
-count lenght of word, divide by 2
-switch around and concatinate
+#count lenght of word, divide by 2
+#switch around and concatinate
 
 s=(input("Enter a string: "))
+x = len(s) / 2
+print(x)
