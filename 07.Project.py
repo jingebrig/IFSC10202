5e9d8986792bdbad820f764b46edf578ddadb108
jingebrig <jingebrig@ualr.edu>
2025-10-06 17:12:25 +0000

    update

---
+++ 07.Project.py
@@ -0,0 +1,6 @@
+inputfile = "07.Project Angles Input.txt"
+outputfile = "07.Project Angles Output.txt"
+recordcount = 0
+
+angleinputfile = open(inputfile,'r')
+angleoutputfile = open(outputfile, 'w')
